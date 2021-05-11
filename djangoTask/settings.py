03ef57e988474fa95569a44d9bafdd2e7b5f5c85
Chinedu Eze <ochuzor@gmail.com>
2021-05-11 23:14:44 +0300

    user login and retrieval of user data

--- djangoTask/settings.py
+++ djangoTask/settings.py
@@ -71,19 +71,15 @@ TEMPLATES = [
     },
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ]
-}
-
 WSGI_APPLICATION = 'djangoTask.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+        'rest_framework.authentication.SessionAuthentication',
+    ),
 }
 
 # Database
