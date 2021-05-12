644f05b921b0284dcd77d86d5764c88943d62947
Chinedu Eze <ochuzor@gmail.com>
2021-05-12 08:44:06 +0300

    use .env for SECRET_KEY; modified todo model

--- todoApp/models.py
+++ todoApp/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class TodoItem(models.Model):
     title = models.CharField(max_length=100)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_by = models.ForeignKey('auth.User', related_name='todos', on_delete=models.CASCADE)
     is_done = models.BooleanField(default=False)
 
     def __str__(self):
